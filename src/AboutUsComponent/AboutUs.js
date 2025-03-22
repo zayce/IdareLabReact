a21a727325d09724305e734e73ad8aa2d2146bab
Zakir <zakir@example.com>
2025-03-22 13:20:14 +0400

    Changed RainbowBaner.js  language in three languages, fixed error class to className

--- src/AboutUsComponent/AboutUs.js
+++ src/AboutUsComponent/AboutUs.js
@@ -8,109 +8,109 @@ export const AboutUs = () => {
 
   return (
     <>
-      <div class="IdareLab-AboutUs-Baner-wrapper">
-        <div class="IdareLab-AboutUs-Baner-inner">
-          <div class="IdareLab-AboutUs-Baner-Title">
-            <div class="IdareLab-AboutUs-Baner-Title-Name">
-              <div class="IdareLab-AboutUs-Baner-Name">
+      <div className="IdareLab-AboutUs-Baner-wrapper">
+        <div className="IdareLab-AboutUs-Baner-inner">
+          <div className="IdareLab-AboutUs-Baner-Title">
+            <div className="IdareLab-AboutUs-Baner-Title-Name">
+              <div className="IdareLab-AboutUs-Baner-Name">
                 {translations[language].aboutUs}
               </div>
-              <div class="IdareLab-AboutUs-Baner-Title-Img">
+              <div className="IdareLab-AboutUs-Baner-Title-Img">
                 <img src={Log} />
               </div>
             </div>
-            <div class="IdareLab-AboutUs-Baner-Desc">
+            <div className="IdareLab-AboutUs-Baner-Desc">
               {translations[language].WithABlend}
             </div>
           </div>
-          <div class="IdareLab-AboutUs-Baner-Objects">
-            <div class="IdareLab-AboutUs-Baner-Object">
-              <div class="IdareLab-AboutUs-Baner-Object-Name">
+          <div className="IdareLab-AboutUs-Baner-Objects">
+            <div className="IdareLab-AboutUs-Baner-Object">
+              <div className="IdareLab-AboutUs-Baner-Object-Name">
                 {translations[language].WhoWeAre}
               </div>
-              <div class="IdareLab-AboutUs-Baner-Object-Desc">
+              <div className="IdareLab-AboutUs-Baner-Object-Desc">
                 {translations[language].AYoungMarketing}
               </div>
             </div>
-            <div class="IdareLab-AboutUs-Baner-Object">
-              <div class="IdareLab-AboutUs-Baner-Object-Name">
+            <div className="IdareLab-AboutUs-Baner-Object">
+              <div className="IdareLab-AboutUs-Baner-Object-Name">
                 {translations[language].OurApproach}
               </div>
-              <div class="IdareLab-AboutUs-Baner-Object-Desc">
+              <div className="IdareLab-AboutUs-Baner-Object-Desc">
                 {translations[language].WeStay}
               </div>
             </div>
-            <div class="IdareLab-AboutUs-Baner-Object">
-              <div class="IdareLab-AboutUs-Baner-Object-Name">
+            <div className="IdareLab-AboutUs-Baner-Object">
+              <div className="IdareLab-AboutUs-Baner-Object-Name">
                 {translations[language].OurGoal}
               </div>
-              <div class="IdareLab-AboutUs-Baner-Object-Desc">
+              <div className="IdareLab-AboutUs-Baner-Object-Desc">
                 {translations[language].ToHelp}
               </div>
             </div>
-            <div class="IdareLab-AboutUs-Baner-Object">
-              <div class="IdareLab-AboutUs-Baner-Object-Name">
+            <div className="IdareLab-AboutUs-Baner-Object">
+              <div className="IdareLab-AboutUs-Baner-Object-Name">
                 {translations[language].OurValues}
               </div>
-              <ul class="IdareLab-AboutUs-Baner-Object-Menu-Items">
-                <li class="IdareLab-AboutUs-Baner-Object-Menu-Item">
-                  <span class="IdareLab-AboutUs-Baner-Object-Menu-Item-bond">
+              <ul className="IdareLab-AboutUs-Baner-Object-Menu-Items">
+                <li className="IdareLab-AboutUs-Baner-Object-Menu-Item">
+                  <span className="IdareLab-AboutUs-Baner-Object-Menu-Item-bond">
                     {translations[language].OurValues1}
                   </span>
-                  <span class="IdareLab-AboutUs-Baner-Object-Menu-Item-normal">
+                  <span className="IdareLab-AboutUs-Baner-Object-Menu-Item-normal">
                     {translations[language].OurValues4}
                   </span>
                 </li>
-                <li class="IdareLab-AboutUs-Baner-Object-Menu-Item">
-                  <span class="IdareLab-AboutUs-Baner-Object-Menu-Item-bond">
+                <li className="IdareLab-AboutUs-Baner-Object-Menu-Item">
+                  <span className="IdareLab-AboutUs-Baner-Object-Menu-Item-bond">
                     {translations[language].OurValues2}
                   </span>
-                  <span class="IdareLab-AboutUs-Baner-Object-Menu-Item-normal">
+                  <span className="IdareLab-AboutUs-Baner-Object-Menu-Item-normal">
                     {translations[language].OurValues5}
                   </span>
                 </li>
-                <li class="IdareLab-AboutUs-Baner-Object-Menu-Item">
-                  <span class="IdareLab-AboutUs-Baner-Object-Menu-Item-bond">
+                <li className="IdareLab-AboutUs-Baner-Object-Menu-Item">
+                  <span className="IdareLab-AboutUs-Baner-Object-Menu-Item-bond">
                     {translations[language].OurValues3}
                   </span>
-                  <span class="IdareLab-AboutUs-Baner-Object-Menu-Item-normal">
+                  <span className="IdareLab-AboutUs-Baner-Object-Menu-Item-normal">
                     {translations[language].OurValues6}
                   </span>
                 </li>
               </ul>
             </div>
-            <div class="IdareLab-AboutUs-Baner-Object">
-              <div class="IdareLab-AboutUs-Baner-Object-Name">
+            <div className="IdareLab-AboutUs-Baner-Object">
+              <div className="IdareLab-AboutUs-Baner-Object-Name">
                 {translations[language].WhatWeDo}
               </div>
-              <ul class="IdareLab-AboutUs-Baner-Object-Menu-Items">
-                <li class="IdareLab-AboutUs-Baner-Object-Menu-Item">
-                  <span class="IdareLab-AboutUs-Baner-Object-Menu-Item-normal">
+              <ul className="IdareLab-AboutUs-Baner-Object-Menu-Items">
+                <li className="IdareLab-AboutUs-Baner-Object-Menu-Item">
+                  <span className="IdareLab-AboutUs-Baner-Object-Menu-Item-normal">
                     {translations[language].BrandCreationAndDevelopment}
                   </span>
                 </li>
-                <li class="IdareLab-AboutUs-Baner-Object-Menu-Item">
-                  <span class="IdareLab-AboutUs-Baner-Object-Menu-Item-normal">
+                <li className="IdareLab-AboutUs-Baner-Object-Menu-Item">
+                  <span className="IdareLab-AboutUs-Baner-Object-Menu-Item-normal">
                     {translations[language].SocialMediaManagement}
                   </span>
                 </li>
-                <li class="IdareLab-AboutUs-Baner-Object-Menu-Item">
-                  <span class="IdareLab-AboutUs-Baner-Object-Menu-Item-normal">
+                <li className="IdareLab-AboutUs-Baner-Object-Menu-Item">
+                  <span className="IdareLab-AboutUs-Baner-Object-Menu-Item-normal">
                     {translations[language].PhotoAndVideoContentProduction}
                   </span>
                 </li>
-                <li class="IdareLab-AboutUs-Baner-Object-Menu-Item">
-                  <span class="IdareLab-AboutUs-Baner-Object-Menu-Item-normal">
+                <li className="IdareLab-AboutUs-Baner-Object-Menu-Item">
+                  <span className="IdareLab-AboutUs-Baner-Object-Menu-Item-normal">
                     {translations[language].CreativeAndAdvertisingPostDesign}
                   </span>
                 </li>
               </ul>
             </div>
-            <div class="IdareLab-AboutUs-Baner-Object">
-              <div class="IdareLab-AboutUs-Baner-Object-Name">
+            <div className="IdareLab-AboutUs-Baner-Object">
+              <div className="IdareLab-AboutUs-Baner-Object-Name">
                 {translations[language].OurPlans}
               </div>
-              <div class="IdareLab-AboutUs-Baner-Object-Desc">
+              <div className="IdareLab-AboutUs-Baner-Object-Desc">
                 {translations[language].WeAre}
               </div>
             </div>
--- src/RainbowBanerComponent/RainbowBaner.js
+++ src/RainbowBanerComponent/RainbowBaner.js
@@ -3,56 +3,50 @@ import "./RainbowBaner.scss";
 import First from "./First.png";
 import Second from "./Second.png";
 import Third from "./Third.png";
+import { useMyContext } from "../UseContext";
 
 export const RainbowBaner = () => {
+  const { translations, language } = useMyContext();
   return (
     <>
-      <div class="IdareLab-Rainbow-Baner-wrapper">
-        <div class="IdareLab-Rainbow-Baner-Inner">
-          <div class="IdareLab-Rainbow-Baner-FirstBLock">
-            <div class="IdareLab-Rainbow-Baner-FirstBLock-Opis">
-              <div class="IdareLab-Rainbow-Baner-FirstBLock-Opis-Name">
-                Experienced Team of Professionals
+      <div className="IdareLab-Rainbow-Baner-wrapper">
+        <div className="IdareLab-Rainbow-Baner-Inner">
+          <div className="IdareLab-Rainbow-Baner-FirstBLock">
+            <div className="IdareLab-Rainbow-Baner-FirstBLock-Opis">
+              <div className="IdareLab-Rainbow-Baner-FirstBLock-Opis-Name">
+                {translations[language].ExperiencedTeamProfessionals}
               </div>
-              <div class="IdareLab-Rainbow-Baner-FirstBLock-Opis-Desc">
-                Our team consists of skilled programmers, designers, video
-                editors, and photographers who bring years of hands-on
-                experience to every project. We combine creativity and technical
-                expertise to deliver outstanding results.
+              <div className="IdareLab-Rainbow-Baner-FirstBLock-Opis-Desc">
+                {translations[language].Ourteamconsists}
               </div>
             </div>
-            <div class="IdareLab-Rainbow-Baner-FirstBLock-Opis-Img">
+            <div className="IdareLab-Rainbow-Baner-FirstBLock-Opis-Img">
               <img src={First} />
             </div>
           </div>
-          <div class="IdareLab-Rainbow-Baner-SecondBLock">
-            <div class="IdareLab-Rainbow-Baner-SecondBLock-Opis">
-              <div class="IdareLab-Rainbow-Baner-SecondBLock-Opis-Name">
-                Client-Oriented Approach
+          <div className="IdareLab-Rainbow-Baner-SecondBLock">
+            <div className="IdareLab-Rainbow-Baner-SecondBLock-Opis">
+              <div className="IdareLab-Rainbow-Baner-SecondBLock-Opis-Name">
+                {translations[language].ClientOriented}
               </div>
-              <div class="IdareLab-Rainbow-Baner-SecondBLock-Opis-Desc">
-                We prioritize your needs and vision. By focusing on personalized
-                solutions and open communication, we ensure that each project
-                aligns with your goals and exceeds your expectations.
+              <div className="IdareLab-Rainbow-Baner-SecondBLock-Opis-Desc">
+                {translations[language].WePrioritizeYourNeedsVision}
               </div>
             </div>
-            <div class="IdareLab-Rainbow-Baner-SecondBLock-Opis-Img">
+            <div className="IdareLab-Rainbow-Baner-SecondBLock-Opis-Img">
               <img src={Second} />
             </div>
           </div>
-          <div class="IdareLab-Rainbow-Baner-ThirdBLock">
-            <div class="IdareLab-Rainbow-Baner-ThirdBLock-Opis">
-              <div class="IdareLab-Rainbow-Baner-ThirdBLock-Opis-Name">
-                Proven Quality and Reliability
+          <div className="IdareLab-Rainbow-Baner-ThirdBLock">
+            <div className="IdareLab-Rainbow-Baner-ThirdBLock-Opis">
+              <div className="IdareLab-Rainbow-Baner-ThirdBLock-Opis-Name">
+                {translations[language].ProvenQualityAndReliability}
               </div>
-              <div class="IdareLab-Rainbow-Baner-ThirdBLock-Opis-Desc">
-                With a strong background working in top companies and real-world
-                projects, we know how to deliver high-quality work on time.
-                Trust us to provide reliable solutions that make a lasting
-                impact.
+              <div className="IdareLab-Rainbow-Baner-ThirdBLock-Opis-Desc">
+                {translations[language].WithAStrong}
               </div>
             </div>
-            <div class="IdareLab-Rainbow-Baner-ThirdBLock-Opis-Img">
+            <div className="IdareLab-Rainbow-Baner-ThirdBLock-Opis-Img">
               <img src={Third} />
             </div>
           </div>
